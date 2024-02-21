ca51f948096e85dd69182f5168a47d6469b98f15
Brando Vidal <yisusdracon@gmail.com>
2024-02-21 03:27:17 -0500

    chore: 🔖  rename validation Course

--- src/Context/Backoffice/Course/domain/CourseName.ts
+++ src/Context/Backoffice/Course/domain/CourseName.ts
@@ -1,15 +1,23 @@
 import { StringValueObject } from '@Shared/domain/value-object/StringValueObject'
 import { CourseNameLengthNeeded } from './errors/CourseNameLengthNeeded'
+import { CourseNameLengthExceded } from './errors/CourseNameLengthExceded'
 
 export class CourseName extends StringValueObject {
   constructor (value: string) {
     super(value)
-    this.ensureLengthIsMoreThanFiveCharacters(value)
+    this.ensureLengthIsLessThanFiveNeeded(value)
+    this.ensureLengthIsMoreThanOneHundredNeeded(value)
   }
 
-  private ensureLengthIsMoreThanFiveCharacters (value: string): void {
+  private ensureLengthIsLessThanFiveNeeded (value: string): void {
     if (value.length < 5) {
       throw new CourseNameLengthNeeded(`The course name <${value}> has less than 5 characters`)
+    }
+  }
+
+  private ensureLengthIsMoreThanOneHundredNeeded (value: string): void {
+    if (value.length > 100) {
+      throw new CourseNameLengthExceded(`The course name <${value}> has more than 50 characters`)
     }
   }
 }
